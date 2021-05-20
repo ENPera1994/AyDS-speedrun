f34a368fd5735848d0eafd96ce98cbed281ebe38
AgustinCalderon <agustincalderon058@gmail.com>
2021-05-20 12:25:56 -0300

    Corrected and association between surveys and careers.  Added asociations for careers and choices models

--- models/career.rb
+++ models/career.rb
@@ -1,3 +1,4 @@
 class Career < Sequel::Model  
-
+	one_to_many :outcomes
+	many_to_many :surveys
 end 
--- models/choice.rb
+++ models/choice.rb
@@ -1,3 +1,5 @@
 class Choice < Sequel::Model  
-
+	one_to_many :outcomes
+	one_to_many :responses
+	many_to_one :questions
 end 
