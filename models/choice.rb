77df146860263a66323213bd20d6d972ae36d5fa
AgustinCalderon <agustincalderon058@gmail.com>
2021-05-27 00:58:27 -0300

    Added validations to choice model

--- models/choice.rb
+++ models/choice.rb
@@ -1,5 +1,10 @@
-class Choice < Sequel::Model  
+class Choice < Sequel::Model
 	one_to_many :outcomes
 	one_to_many :responses
 	many_to_one :questions
+
+	def validate
+		super
+		errors.add(:text, 'cannot be empty') if !text || text.empty?
+	end
 end 
