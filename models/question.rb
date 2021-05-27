dca33a181f8416faf72806a7b8b4035e6d7802fe
AgustinCalderon <agustincalderon058@gmail.com>
2021-05-27 01:05:32 -0300

    Added validations to question model

--- models/question.rb
+++ models/question.rb
@@ -1,4 +1,14 @@
-class Question < Sequel::Model  
+class Question < Sequel::Model
 	one_to_many :choices
-    one_to_many :responses
+  one_to_many :responses
+
+	def validate
+		super
+		errors.add(:name, 'cannot be empty') if !name || name.empty?
+	end
+
+	def validate
+		super
+		errors.add(:description, 'cannot be empty') if !description || description.empty?
+	end
 end 
