7b1136a9ed0125556ea209ea82612389dd7f957c
AgustinCalderon <agustincalderon058@gmail.com>
2021-05-27 02:07:11 -0300

    Added tests for question

---
+++ tests/models/questions_test.rb
@@ -0,0 +1,20 @@
+require File.expand_path '../../test_helper.rb', __FILE__
+
+class QuestionTest < MiniTest::Unit::TestCase
+  MiniTest::Unit::TestCase
+
+  def test_question_must_has_name
+    # Arrange
+    question1 = Question.new(name: '')
+    question2 = Question.new(name: nil)
+    question3 = Question.new(name: 'I am a question?')
+    # Act
+
+
+    # Assert
+    assert_equal(question1.valid?, false)
+    assert_equal(question2.valid?, false)
+    #assert_equal(question3.valid?, true)
+  end
+
+end
