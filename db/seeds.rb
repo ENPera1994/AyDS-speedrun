fc0ce1a1bd7a007a12abb13f3fa9ba760b84996d
Eduardo Pera <eduardo.pera1994@gmail.com>
2021-06-04 20:04:01 -0300

    cargadas todas las carreras del ejemplo del profe

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
 #Clean Dataset
-Survey.all.map { |s| s.detroy }
-Career.all.map { |c| c.detroy }
+Survey.all.map { |s| s.destroy }
+Career.all.map { |c| c.destroy }
+Choice.all.map { |c| c.destroy }
+Outcome.all.map { |c| c.destroy }
+Question.all.map { |c| c.destroy }
+Response.all.map { |c| c.destroy }
 
 #Create Careers
-Career.create(name: 'Computacion', description: 'Duracion 5 años, https://www.unrc.edu.ar/unrc/carreras/ProgAsig2.php?n=BatVf5iHwU0Fx5G7YNW8kNLQ%2BhOdQH4W7AWg7BMXHW8%3D')
-Career.create(name: 'Veterinaria', description: 'Duracion 5 años y medio, https://www.unrc.edu.ar/unrc/carreras/ayv_medicina_veterinaria.htm')
+Career.create(name: 'Computacion', description: ' ')
+Career.create(name: 'Veterinaria', description: ' ')
+Career.create(name: 'Agronomia', description: ' ')
+Career.create(name: 'Arquitectura', description: ' ')
+Career.create(name: 'Arte', description: ' ')
+Career.create(name: 'Economía', description: ' ')
+Career.create(name: 'Educación física', description: ' ')
+Career.create(name: 'Filosofía', description: ' ')
+Career.create(name: 'Física', description: ' ')
+Career.create(name: 'Geología', description: ' ')
+Career.create(name: 'Historia', description: ' ')
+Career.create(name: 'Literaruta', description: ' ')
+Career.create(name: 'Matemáticas', description: ' ')
+Career.create(name: 'Medicina', description: ' ')
+Career.create(name: 'Periodismo', description: ' ')
+Career.create(name: 'Química', description: ' ')
+Career.create(name: 'Sociología', description: ' ')
