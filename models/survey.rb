3f5f909778b0358db2e50ce0d01b9f7d4441bf32
nachoo <nachogonz2702@yahoo.com.ar>
2021-09-08 19:11:43 -0300

    Added functions for calculate result of survey and get scores in a hashmap

--- models/survey.rb
+++ models/survey.rb
@@ -13,4 +13,36 @@ class Survey < Sequel::Model
       response.destroy
     end
   end
+
+  def result   #return an array with Career object, the best career and any other tied in score
+    topCareers = Array.new #array for best careers
+    hashCareer = self.career_scores   #obtain scores
+    maxScore = hashCareer.values.max  #score of best career
+    
+    if maxScore.nil? #case of empty hashmap (no career scored)
+      topCareers[0] = Career.first  #null career
+      return topCareers
+    end
+
+    #filter the hash leaving only the best and all careers tied in score with the best one
+    hashCareer.select! {|key,value| value == maxScore}
+    
+    hashCareer.keys.each do |career_id| #for every career id in hash
+      topCareers.append(Career.find(id: career_id)) #load Career object with this id
+    end
+    return topCareers
+  end
+
+  def career_scores   #return a hashmap with career_id as key and score as value
+    hashCareer = Hash.new(0)  #create a hashmap for careers score initialized with 0
+    
+    for response in self.responses   #for every response to the current survey
+			choice = Choice.find(id: response.choice_id)    #choice selected as response
+			for outcome in choice.outcomes      #for every outcome of the selected choices
+				hashCareer[outcome.career_id] += 1   #add 1 point to the career
+			end
+		end
+    
+    return hashCareer   #if no career has scored, we get an emtpy hash
+  end
 end
