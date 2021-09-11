a76a5d4d67103f60a408cb382876f8b3fc4d0595
AgustinCalderon <agustincalderon058@gmail.com>
2021-09-11 01:50:24 -0300

    Added test for careers

--- tests/models/careers_test.rb
+++ tests/models/careers_test.rb
@@ -15,4 +15,43 @@ class CareerTest < MiniTest::Unit::TestCase
     # Assert
     assert_equal(career.surveys.count, 3)
   end
+
+  def test_career_has_many_outcomes
+
+    #Arrange
+    career = Career.create(name: 'Licenciado en Test')
+    question = Question.create(name: 'test', description: 'test', number: 1, type: 'test')
+    choice = Choice.create(text: 'la choice', question_id: question.id)
+    outcome = Outcome.create(career_id: career.id, choice_id: choice.id)
+    outcome2 = Outcome.create(career_id: career.id, choice_id: choice.id)
+    outcome3 = Outcome.create(career_id: career.id, choice_id: choice.id)
+    #Act
+
+    #Assert
+    assert_equal(career.outcomes.count, 3)
+  end
+
+  def test_career_has_name_no_null
+   
+    # Arrange
+    career = Career.new
+
+    # Act
+    career.name = nil
+
+    # Assert
+    assert_equal(career.valid?, false)
+  end
+
+  def test_career_has_name_no_empty
+   
+    # Arrange
+    career = Career.new
+
+    # Act
+    career.name = ''
+
+    # Assert
+    assert_equal(career.valid?, false)
+  end
 end
