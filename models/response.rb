aa9e727168576deb2a6c77ce623a9908fd1b1fac
AgustinCalderon <agustincalderon058@gmail.com>
2021-09-11 00:47:58 -0300

    Added validations to response model

--- models/response.rb
+++ models/response.rb
@@ -2,4 +2,11 @@ class Response < Sequel::Model
 	many_to_one :survey
 	many_to_one :question
 	many_to_one :choice
+
+	def validate
+        super
+        errors.add(:survey_id, 'cannot be empty')if !survey_id  
+        errors.add(:question_id, 'cannot be empty')if !question_id 
+        errors.add(:choice_id, 'cannot be empty')if !choice_id 
+    end
 end
