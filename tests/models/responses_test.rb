fd0d70ae3521933528fea0297f9ab07331e4b5bc
AgustinCalderon <agustincalderon058@gmail.com>
2021-09-11 15:25:52 -0300

    Change in test for response

--- tests/models/responses_test.rb
+++ tests/models/responses_test.rb
@@ -4,43 +4,25 @@ require File.expand_path '../../test_helper.rb', __FILE__
 class ResponseTest < MiniTest::Unit::TestCase
   MiniTest::Unit::TestCase  
 
-  def test_response_must_has_survey
-    
-    # Arrange
-    survey = Survey.new
-    response = Response.new    
+  def test_response_must_has_choice_question_and_survey
 
-    # Act
-    response.survey_id = survey.id
+    career = Career.create(name: 'test')
+    question = Question.create(name: 'name_est', description: 'test', number: 1, type: 'test')
+    choice = Choice.create(text: 'choice_test', question_id: question.id)
+    survey = Survey.create(username: 'Pepe', career_id: career.id)
+    response = Response.new
 
-    # Assert
-    assert_equal(response.valid?, false)
-  end
-
-    def test_response_must_has_question
-    
-    # Arrange
-    question = Question.new
-    response = Response.new    
-
-    # Act
-    response.question_id = question.id 
-
-    # Assert
-    assert_equal(response.valid?, false)
-  end
-   
-  def test_response_must_has_choice
-    
-    # Arrange
-    choice = Choice.new
-    response = Response.new    
-
-    # Act
+    #Response has choice_id but not question_id and survey_id
     response.choice_id = choice.id 
-
-    # Assert
     assert_equal(response.valid?, false)
+
+    #Response has choice_id and question_id but not survey_id
+    response.question_id = question.id
+    assert_equal(response.valid?, false) 
+    
+    #Response has choice_id, question_id and survey_id
+    response.survey_id = survey.id
+    assert_equal(response.valid?, true)
   end
 
 end
