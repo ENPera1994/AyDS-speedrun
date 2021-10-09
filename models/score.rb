b1246bb2010218f96017a904c4d434a218328811
nachoo <nachogonz2702@yahoo.com.ar>
2021-10-09 18:36:34 -0300

    [#179818757] Corrected an association issue and added a method for creating scores

--- models/score.rb
+++ models/score.rb
@@ -1,5 +1,19 @@
 class Score < Sequel::Model  
-  one_to_many :careers
-  one_to_many :surveys
+  many_to_one :careers
+  many_to_one :surveys
+
+  #takes a collection of careers and an id and creates a Score object with
+  #the given survey_id and career, for each career in the collection 
+  #self makes the method static so we don't need an instance to invoke it
+  def self.create_scores(careers, survey_id)
+    careers.each do |career|
+      score = Score.new(career_id: career.id, survey_id: survey_id)
+      if score.save  #store score in database
+		    [201, {'Location' => "scores/#{score.id}"}, 'Score succesfully created']
+		  else
+		    [500, {}, 'Internal Server Error']
+		  end
+    end
+  end
 
 end
