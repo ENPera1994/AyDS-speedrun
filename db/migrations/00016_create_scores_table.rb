a88805b78435c4446f815ff9c6070d8a9cf1bdab
nachoo <nachogonz2702@yahoo.com.ar>
2021-10-09 16:19:40 -0300

    [#179818758] migration for creating scores table

---
+++ db/migrations/00016_create_scores_table.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  up do
+    create_table(:scores) do
+      primary_key :id
+      foreign_key :career_id, :careers
+      foreign_key :survey_id, :surveys
+      DateTime      :created_at,   default: Sequel::CURRENT_TIMESTAMP
+      #updated at DateTime will not be necesary, since we don't want scores to be modified
+    end
+  end
+
+  down do
+    drop_table(:scores)
+  end
+end
