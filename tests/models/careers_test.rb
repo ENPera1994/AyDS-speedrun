0ee2438a4604b62e300812839d723f376b1a0f78
AgustinCalderon <agustincalderon058@gmail.com>
2021-10-11 17:46:47 -0300

    [#179859204] Modified test for career

--- tests/models/careers_test.rb
+++ tests/models/careers_test.rb
@@ -3,17 +3,17 @@ require File.expand_path '../../test_helper.rb', __FILE__
 class CareerTest < MiniTest::Unit::TestCase
   MiniTest::Unit::TestCase
 
-  def test_career_has_many_surveys
+  def test_career_has_many_score
     # Arrange
     career = Career.create(name: 'computacion')
 
     # Act
-    Survey.create(username: '1', career_id: career.id)
-    Survey.create(username: '2', career_id: career.id)
-    Survey.create(username: '3', career_id: career.id)
+    Score.create(career_id: career.id)
+    Score.create(career_id: career.id)
+    Score.create(career_id: career.id)
 
     # Assert
-    assert_equal(career.surveys.count, 3)
+    assert_equal(career.score.count, 3)
   end
 
   def test_career_has_many_outcomes
