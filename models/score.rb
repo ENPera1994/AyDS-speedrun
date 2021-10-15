8c1dd4ff6750cb35a09b4f87e3fde37c4fa1b635
nachoo <nachogonz2702@yahoo.com.ar>
2021-10-15 19:16:37 -0300

    [finishes #179818817] New method to make the query

--- models/score.rb
+++ models/score.rb
@@ -26,4 +26,18 @@ class Score < Sequel::Model
     return careers
   end
 
+  def self.count_query(first_date, last_date, career_name)
+    #if any of the parameters is not provided we return null
+    if first_date.nil? || last_date.nil? || career_name.nil? || career_name == "Carrera"
+      return nil
+    end
+
+    parsed_fst_d = Date.parse(first_date) #get first date as Date
+    parsed_lst_d = Date.parse(last_date)  #get last date as Date
+    career_id = Career.find(name: career_name).id
+    
+    query = Score.where(:created_at => parsed_fst_d .. parsed_lst_d).where(:career_id => career_id)
+    #SELECT * FROM scores WHERE ((career_id = career_id) AND (created_at BETWEEN parsed_fst_d AND parsed_lst_d))
+    return query.count.to_s #lastly we make a count of the rows in query
+  end
 end
