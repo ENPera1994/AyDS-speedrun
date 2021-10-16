2f3005421640464b549d7b62d65e9677c64f36e0
AgustinCalderon <agustincalderon058@gmail.com>
2021-10-16 11:05:35 -0300

    Fixed test for score

--- tests/models/scores_test.rb
+++ tests/models/scores_test.rb
@@ -1,30 +1,14 @@
-require File.expand_path '../../test_helper.rb', __FILE__
+require File.expand_path '../../test_helper.rb', FILE
 
 class ScoreTest < MiniTest::Unit::TestCase
     MiniTest::Unit::TestCase
 
-    def test_score_has_a_career
-        
-        career = Career.create(name: 'Lic in tests')
-        score = Score.create(career_id: career.id)
-        
-        assert_equal(score.valid?, false)
-    end
-
-    def test_score_has_a_survey
-        
-        career = Career.create(name: 'Lic in tests')
-        survey = Survey.create(username: 'Test')
-        score = Score.crate(survey_id: survey.id)
-
-        assert_equal(score.valid?, false) 
-    end
-
     def test_score_has_a_survey_and_career
-        
+
         career = Career.create(name: 'Lic in tests')
         survey = Survey.create(username: 'Test')
-        score = Score.crate(career_id: career.id, survey_id: survey.id)
+        score = Score.create(career_id: career.id, survey_id: survey.id)
 
         assert_equal(score.valid?, true) 
     end
+end
